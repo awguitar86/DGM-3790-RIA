7ab7882d7c0dd9daca70a51022141f49a3c585d0
Austin Wright <10798487@uvu.edu>
2019-09-13 14:38:22 -0600

    Worked through more of Section 8 on Udemy

--- Section-8/src/components/User.vue
+++ Section-8/src/components/User.vue
@@ -3,13 +3,24 @@
         <h1>The User Component</h1>
         <p>I'm an awesome User!</p>
         <button @click="changeName">Change my Name</button>
+        <p>Name is {{name}}</p>
+        <p>Age is {{age}}</p>
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <appUserDetail :name="name" @nameWasReset="name = $event"></appUserDetail>
+                <appUserDetail
+                    :name="name"
+                    @nameWasReset="name = $event"
+                    :userAge="age"
+                >
+                </appUserDetail>
             </div>
             <div class="col-xs-12 col-sm-6">
-                <appUserEdit></appUserEdit>
+                <appUserEdit
+                    :userAge="age"
+                    @ageWasEdited="age = $event"
+                >
+                </appUserEdit>
             </div>
         </div>
     </div>
@@ -22,13 +33,14 @@
     export default {
         data: function() {
             return {
-                name: 'Max'
+                name: 'Max',
+                age: 27
             }
         },
         methods: {
             changeName(){
                 this.name = 'Anna'
-            }
+            },
         },
         components: {
             appUserDetail: UserDetail,
--- Section-8/src/components/UserDetail.vue
+++ Section-8/src/components/UserDetail.vue
@@ -3,18 +3,17 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>User Name: {{ name }}</p>
-        <button @click="resetName">Reset</button>
+        <p>User Age: {{ userAge }}</p>
+        <button @click="resetName">Reset Name</button>
     </div>
 </template>
 
 <script>
+import {eventBus} from '../main';
     export default {
         props: {
-            name: {
-                type: String,
-                required: true, //component will only work if this is type string
-                // default: 'Max'
-            }
+          name: String,
+          userAge: Number
         },
         methods: {
             // switchName(){
@@ -24,6 +23,11 @@
                 this.name = 'Max';
                 this.$emit('nameWasReset', this.name);
             }
+        },
+        created() {
+          eventBus.$on('ageWasEdited', (age) => {
+              this.userAge = age;
+          });
         }
     }
 </script>
