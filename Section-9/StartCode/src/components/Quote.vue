dfd1a0b5c6994f84ffc418ad9da930f7ad314440
Austin Wright <10798487@uvu.edu>
2019-09-27 13:05:11 -0600

    added section 9 start code

---
+++ Section-9/StartCode/src/components/Quote.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p>A Wonderful Quote!</p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="stylus" scoped>
+    div {
+        border: solid 1px #ccc;
+        box-shadow: 1px 1px 2px black;
+        padding: 30px;
+        margin: auto;
+    }
+</style>
