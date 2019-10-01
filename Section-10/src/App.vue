171a7208a6f81ca0597f7a69413e3b1f4b9320aa
Austin Wright <10798487@uvu.edu>
2019-10-01 14:44:18 -0600

    finished ection 10 Quote project

---
+++ Section-10/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+        <Header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></Header>
+        <NewQuote @quoteAdded="newQuote"></NewQuote>
+        <QuoteGrid :quotes="quotes" @quoteDeleted="deleteQuote"></QuoteGrid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Info: Click a Quote to delete it!</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
+    import Header from './components/Header.vue';
+
+    export default {
+        data: function() {
+            return {
+                quotes: [
+                    'Just a quote to see something'
+                ],
+                maxQuotes: 10
+            }
+        },
+        methods: {
+            newQuote(quote) {
+                if (this.quotes.length >= this.maxQuotes) {
+                    return alert('Please delete Quotes first!');
+                }
+                this.quotes.push(quote);
+            },
+            deleteQuote(index){
+                this.quotes.splice(index, 1);
+            }
+        },
+        components: {
+            QuoteGrid,
+            NewQuote,
+            Header
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ Section-10/src/components/NewQuote.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="row quote-body">
+        <form action="">
+            <div class="col-sm-8 col-md-offset-2 col-xs-12 col-md-6 col-md-offset-3">
+                    <label>Quote</label>
+                    <textarea class="form-control" rows="3" v-model="quote"></textarea>
+            </div>
+             <div class="col-sm-8 col-md-offset-2 col-xs-12 col-md-6 col-md-offset-3">
+                    <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                quote: ''
+            }
+        },
+        methods: {
+            createNew() {
+                this.$emit('quoteAdded', this.quote);
+                this.quote = '';
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .quote-body {
+        margin-bottom: 20px;
+    }
+    button {
+        margin-top: 10px;
+    }
+</style>
