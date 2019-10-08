700d961548c6140cc3974e527938bc603ee3b7d0
Austin Wright <10798487@uvu.edu>
2019-10-08 13:05:04 -0600

    completed section 14 and mini app quiz animation app

---
+++ Section-14/MiniApp/src/components/Answer.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="alert alert-success text-center" key="answer">
+        <h1>That's Correct!</h1>
+        <hr>
+        <button class="btn btn-primary" @click="onNextQuestion">Next Question</button>
+    </div>
+</template>
+<style>
+
+</style>
+<script>
+
+    export default{
+        methods: {
+            onNextQuestion() {
+                this.$emit('confirmed');
+            }
+        }
+    }
+</script>
