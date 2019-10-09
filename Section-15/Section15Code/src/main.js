e3bcdb8efc202ea80434ffe8d06ac9b9362248dc
Austin Wright <10798487@uvu.edu>
2019-10-09 17:06:45 -0600

    finished section 15

--- Section-15/Section15Code/src/main.js
+++ Section-15/Section15Code/src/main.js
@@ -1,5 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource';
+
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://vue-http-dd206.firebaseio.com/';
+Vue.http.interceptors.push((req, next) => {
+  console.log(req);
+  if(req.method == "POST"){
+    req.method = "PUT";
+  }
+  next(res => {
+    res.json = () => {
+      return {messages: res.body}
+    }
+  });
+});
 
 new Vue({
   el: '#app',
